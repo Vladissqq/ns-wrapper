6af0d6cfe1f0bdf8604f4820296c3dedfa0a75b7
vladissqq <vladissqq@gmail.com>
2020-09-02 14:18:09 +0300

    add some access strings to app.tns

--- src/app/app.component.tns.ts
+++ src/app/app.component.tns.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
-import {isAndroid} from "tns-core-modules/platform";
-import {Page} from 'tns-core-modules/ui/page';
-import {WebView} from 'tns-core-modules/ui/web-view';
+import { Component } from '@angular/core';
+import { isAndroid } from "tns-core-modules/platform";
+import { Page } from 'tns-core-modules/ui/page';
+import { WebView } from 'tns-core-modules/ui/web-view';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +19,14 @@ export class AppComponent {
       this.page = <Page>args.object;
       this.webView = <WebView>this.page.getViewById('webView');
       this.webView.android.setInitialScale(this.scale);
+      this.webView.android.getSettings().setDomStorageEnabled(true);
+      this.webView.android.getSettings().setDatabaseEnabled(true);
+      this.webView.android.getSettings().setDisplayZoomControls(false);
+      this.webView.android.getSettings().setBuiltInZoomControls(false);
+      this.webView.android.getSettings().setAllowFileAccessFromFileURLs(true);
+      this.webView.android.getSettings().setAllowUniversalAccessFromFileURLs(true);
+      this.webView.android.getSettings().setMediaPlaybackRequiresUserGesture(false);
+      this.webView.android.getSettings().setUseWideViewPort(true);
     }
   }
 }
